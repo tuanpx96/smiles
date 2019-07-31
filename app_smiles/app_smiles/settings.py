4f315054dc558eb486ddef23b78202ae4354027a
tuanpx <tuan.pham.xuan@ntq-solution.com.vn>
2019-07-31 11:23:04 +0700

    Init app users

--- app_smiles/app_smiles/settings.py
+++ app_smiles/app_smiles/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app_smiles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'smiles_db',
+        'USER': 'smiles_user',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
